660d2f490540e3cd58732a57f6751e89a6e82fe7
Nick Pavlovic <nickpavlovic@icloud.com>
2022-11-08 18:08:06 -0500

    built MVC structure models and routes

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const {Schema,model} = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema({
+
+    thoughtText: {
+        type:String,
+        required:true,
+        maxLength: 280,
+        minLength: 1
+    },
+    createdAt: {
+        type:Date,
+        default:Date.now(),
+        //Use a getter method to format the timestamp on query
+        get: v => v.toLocalDateString()
+    },
+    username: {
+        type:String,
+        required:true
+    },
+    reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+);
+
+thoughtSchema
+.virtual('reactionCount')
+.get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model.apply('thought',thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const {Schema,model} = require('mongoose');
+
+const userSchema = new Schema (
+    {
+      username: {
+        type:String,
+        required: true,
+        trim:true
+      },
+      email: {
+        type: String,
+        unique:true,
+        validate: {
+            validator: function(v) {
+              return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+            },
+            message: props => `${props.value} is not a valid e-mail!`
+          },
+        required: [true, 'User e-mail  required']
+
+      },
+      thoughts: {
+        type:Schema.Types.ObjectId,
+        ref: 'Thought'
+      },
+      friends: {
+        type:Schema.Types.ObjectId,
+        ref: 'User'
+      }
+
+    },
+    {toJSON: {
+        virtuals:true,
+      },
+      id:false,
+    }
+);
+
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  });
+
+  const User=model('user',userSchema);
+
+  module.exports = User;
