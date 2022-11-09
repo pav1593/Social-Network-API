185665c75dbafb6f8c901a30acbf5b08f7c002f4
Nick Pavlovic <nickpavlovic@icloud.com>
2022-11-08 22:56:22 -0500

    added getThoughts controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,7 @@ const {User,Thought} = require('../models');
 module.exports = {
     getThoughts(req,res){
         Thought.find()
-          .then((thoughts) => res.json(thoughts))
+          .then((thought) => res.json(thought))
           .catch ((err)=>res.status(500).json(err));
     },
     getSingleThought(req,res){},
