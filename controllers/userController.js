bc5a5626e1235c916cf16bab0728e0c4bff9ddd6
Nick Pavlovic <nickpavlovic@icloud.com>
2022-11-08 20:40:21 -0500

    added getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,16 @@ getUsers(req,res){
       .then((students) => res.json(students))
       .catch ((err)=>res.status(500).json(err));
 },
-getSingleUser(req,res){},
+getSingleUser(req,res){
+    User.findOne({_id:req.params.userId})
+      .select('-__v')
+      .then((user) => 
+          !user
+            ? res.status(404).json({message:'No user with that ID'})
+            : res.json(user)
+      )
+      .catch ((err)=>res.status(500).json(err));
+},
 createUser(req,res){},
 updateUser(req,res){},
 removeUser(req,res){},
