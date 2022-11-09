74078952c46a25023377233aec71ef017d93daf8
Nick Pavlovic <nickpavlovic@icloud.com>
2022-11-08 22:28:15 -0500

    added createThought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,7 +7,25 @@ module.exports = {
           .catch ((err)=>res.status(500).json(err));
     },
     getSingleThought(req,res){},
-    createThought(req,res){},
+    createThought(req,res){
+        Thought.create(req.body)
+        .then((thought)=>{
+                return User.findOneAndUpdate(
+                    {_id: req.body.userId},
+                    { $addToSet: {thoughts: thought._id}},
+                    { new: true}
+                );
+            })
+            .then((user)=>
+            !user
+                ? res
+                    .status(404)
+                    .json({message: 'Post created, but found no user with that ID'})
+                : res.json('Created the post')
+            )
+        .catch((err)=> res.status(500).json(err));
+        
+    },
     updateThought(req,res){},
     removeThought(req,res){},
     addReaction(req,res){},
